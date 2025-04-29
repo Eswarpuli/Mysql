d867a0a11c95751525c5b2d88e9df1fe0a9f6953
Eswarpuli <eswarpuli04@gmail.com>
2025-04-29 22:43:21 +0530

    Update 2- Select Statement.sql

--- 2- Select Statement.sql
+++ 2- Select Statement.sql
@@ -1,82 +1,47 @@
--- SELECT STATEMENET
+-- SELECT STATEMENT
 
--- the SELECT statement is used to work with columns and specify what columns you want to work see in your output. There are a few other things as well that
--- we will discuss throughout this video
+-- The SELECT statement is used to work with columns and specify what columns you want to see in your output.
 
-#We can also select a specefic number of column based on our requirement. 
-
-#Now remember we can just select everything by saying:
+-- Select everything
 SELECT * 
-FROM parks_and_recreation.employee_demographics;
+FROM Gec.employees;
 
-
-#Let's try selecting a specific column
+-- Select a specific column
 SELECT first_name
-FROM employee_demographics;
+FROM employees;
 
-#As you can see from the output, we only have the one column here now and don't see the others
-
-#Now let's add some more columns, we just need to separate the columns with columns
+-- Select multiple specific columns
 SELECT first_name, last_name
-FROM employee_demographics;
+FROM employees;
 
-#Now the order doesn't normall matter when selecting your columns.
-#There are some use cases we will look at in later modules where the order of the column
-#Names in the select statement will matter, but for this you can put them in any order
-
+-- Order of columns does not matter unless explicitly required
 SELECT last_name, first_name, gender, age
-FROM employee_demographics;
+FROM employees;
 
-#You'll also often see SQL queries formatted like this.
+-- More readable format
 SELECT last_name, 
-first_name, 
-gender, 
-age
-FROM employee_demographics;
+       first_name, 
+       gender, 
+       age
+FROM employees;
 
-#The query still runs the exact same, but it is easier to read and pick out the columns
-#being selected and what you're doing with them.
-
-#For example let's take a look at using a calculation in the select statement
-
-#You can see here we have the total_money_spent - we can perform calculations on this
+-- Using calculations in the SELECT statement (example with employee_salary)
 SELECT first_name,
- last_name,
- total_money_spent,
- total_money_spent + 100
-FROM customers;
+       last_name,
+       salary,
+       salary + 100
+FROM employee_salary;
 
-#See how it's pretty easy to read and to see which columns we are using.
-
-#Math in SQL does follow PEMDAS which stands for Parenthesis, Exponent, Multiplication,
-#Division, Addition, subtraction - it's the order of operation for math
-
-#For example - What will the output be?:
+-- Math operations follow PEMDAS order
 SELECT first_name, 
-last_name,
-salary,
-salary + 100
-FROM employee_salary;
-#This is going to do 10* 100 which is 1000 and then adds the original 540
-
-#Now what will the output be when we do this?
-SELECT first_name, 
-last_name,
-salary,
-(salary + 100) * 10
+       last_name,
+       salary,
+       (salary + 100) * 10
 FROM employee_salary;
 
-
-# Pemdas
-
-#One thing I wanted to show you about the select statement in this lesson is the DISTINCT Statement - this will return only unique values in
-#The output - and you won't have any duplicates
-
-SELECT department_id
+-- Using DISTINCT to get only unique department IDs
+SELECT dept_id
 FROM employee_salary;
 
-SELECT DISTINCT department_id
+SELECT DISTINCT dept_id
 FROM employee_salary;
-
-#Now a lot happens in the select statement. We have an entire module dedicated to just the 
-#select statement so this is kind of just an introduction to the select statement.
